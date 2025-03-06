b93d1b0d296d74352c968f3c2ded1fa3c9a318e9
Rafaelakamexican <r3742127@gmail.com>
2025-03-06 09:57:32 -0800

    i made the tank move
    it went forward

--- U1PlayerControlMuro/Assets/Scripts/PlayerController.cs
+++ U1PlayerControlMuro/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    public float turnSpeed;
+
     // Update is called once per frame
     void Update()
     {
         // We'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }
